62140a46770f1ee71d29954b1fc0611858496799
Peter Wullen <46973667+PWullen@users.noreply.github.com>
2022-07-12 13:50:34 -0700

    Update EE350hw6.m

--- EE350hw6.m
+++ EE350hw6.m
@@ -1,5 +1,17 @@
 %Geometric distribution
 %%Write a function to generate samples of X.
+% A basketball team with 15 players is practicing.  During practice,
+% each player attempts to shoot a basket, one after another. The star
+% player, Prob Ability, always shoots first. Prob’s shot has a 0.5 success
+% probability. Prob is such an inspiration to the rest of the team that if
+% their shot is successful, the remaining players do much better following
+% it. Specifically, if Prob is successful, each of the other players’ shots
+% are successful with probability 0.8, but if Prob is unsuccessful, they
+% are only successful with probability 0.2.  After Prob shoots, all the
+% other player’s shots are independent.  Once each player shoots once,
+% let X be the total number of successful shots (including Prob’s).
+
+
 function EE350hw6(s) %s = samples of experiment
     n = 15; %basketball team players
     ProbP = 0.5; %star player always shooting first 
